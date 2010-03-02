e660547869ac686a5cc6ac92e3d06831b06aa656
unknown <leizhou@.(none)>
2010-03-02 22:56:36 +1100

    finish the part of form

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -148,6 +148,98 @@ $(document).ready(function() {
 //side jquery content start from here
 
 
+/*Date picker */
+datapick_config = function(){
+
+    $('.startdatepick').datepicker({
+       
+    });
+
+    $('.enddatepick').datepicker({
+      
+    });
+
+    $('.datepick').datepicker({
+        showOn: 'button',
+        buttonImage: '/images/calendar.png',
+        buttonImageOnly: true,
+        dateFormat: 'dd-mm-yy',
+        altFormat: 'mm-dd-yy',
+        changeMonth: true,
+        changeYear: true,
+        yearRange: '-200:+20',
+        onClose: function(){
+            $(this).keyup();
+        }
+    });
+
+
+    $('.beforestartdatepick').datepicker({
+        showOn: 'button',
+        buttonImage: '/images/calendar.png',
+        buttonImageOnly: true,
+        dateFormat: 'dd-mm-yy',
+        altFormat: 'mm-dd-yy',
+        changeMonth: true,
+        changeYear: true,
+        yearRange: '-200:+20',
+        beforeShow: function(){
+            var arr_dateText = $("#"+$(this).attr("end_date")).val().split("-");
+            day = arr_dateText[0];
+            month = arr_dateText[1];
+            year = arr_dateText[2];
+            if(year!=undefined){
+                $(this).datepicker('option', 'maxDate', new Date(year, month-1, day));
+            }
+        }
+    });
+
+
+
+
+
+
+
+    $('.ui-datepicker-trigger').live('mouseover', function(){
+        var endDate = $(this).parent().find('.enddatepick');
+        if(endDate.attr('start_date')!=undefined){
+            var arr_dateText = $('#'+endDate.attr('start_date')).val().split("-");
+            year = arr_dateText[2];
+            if(year==undefined){
+                endDate.val('');
+                endDate.datepicker('disable');
+            }
+        }else{
+            var roleEndDate = $(this).parent().find('.role_enddatepick');
+            if(roleEndDate.attr('start_date')!=undefined){
+                var role_arr_dateText = $('#'+roleEndDate.attr('start_date')).val().split("-");
+                year = role_arr_dateText[2];
+                if(year==undefined){
+                    roleEndDate.val('');
+                    roleEndDate.datepicker('disable');
+                }
+            }
+        }
+    });
+};
+
+
+$(document).ready(function(){
+    datapick_config();
+});
+
+
+    $(document).ready(function() {
+      $(':checkbox').iphoneStyle();
+    });
+
+
+
+
+
+
+
+
 
 jQuery(function($){
     // *** search box ***
@@ -216,6 +308,15 @@ jQuery(function($){
     }
     showAddresses(['13-17 Oxford St, Burwood, NSW', 'Hornsey St, Burwood, NSW']);
 });
+
+
+
+
+
+
+
+
+
 
 
 
