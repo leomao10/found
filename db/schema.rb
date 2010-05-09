c9a406e9c3400dabe91dd0fa28bd69dbe0cd5efa
leomao10 <leoliang_climber@hotmail.com>
2010-05-09 15:22:47 +1000

    change the databses style

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100416183702) do
+ActiveRecord::Schema.define(:version => 20100509051256) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "post_id"
@@ -116,6 +116,14 @@ ActiveRecord::Schema.define(:version => 20100416183702) do
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "suburbs", :force => true do |t|
+    t.string   "name"
+    t.string   "state"
+    t.integer  "postcode"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username"
